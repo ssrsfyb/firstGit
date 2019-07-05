df7d5ea9f0cb848fb4391cf895f0c344956bc8d8
fyb <1483824245@qq.com>
2019-07-05 14:23:15 +0800

    第三次展示

--- hahaha/src/main/java/com/itheima/demo01.java
+++ hahaha/src/main/java/com/itheima/demo01.java
@@ -4,5 +4,6 @@ public class demo01 {
     public static void main(String[] args) {
         System.out.println("这是李四的第一次代码");
         System.out.println("这是第二次展示");
+        System.out.println("第三次展示");
     }
 }
